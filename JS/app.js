a00a5fbcf0b1e7dc6e61064a4c85c05e56de72fa
Dark-str <sasithisurutc@gmail.com>
2026-09-23 23:09:52 +0530

    _Lab_#1

---
+++ JS/app.js
@@ -0,0 +1,29 @@
+//document.write("Ja Jaa JAa!");
+/*alert("SYBAU");
+
+var name = "Jo";
+var ssn = 1009384922;
+
+document.write("This is my nigga: "+name);
+document.write("SSN"+" "+ ssn)
+
+var name = "CKSTR";
+var uni = "NSBM";
+var age = 10;
+var city = "Colombo";
+
+document.write("My name is "+name+" and my university is "+uni+". I am "+age+" years old and I live in "+city+".")
+
+var f1 = 10;
+var f2 = 5;
+
+alert("Total : "+ (f1+f2));*/
+
+var guests = prompt("Enter # of Guests: ");
+var food = 15;
+var dec = 150;
+var ent = 300;
+var tot = (food*guests)+ent+dec;
+
+alert("Total: "+ tot);
+confirm("Are those calculations are accurate ASF?")
